b7c3b8cc702953b4547c15d1564f13e411bd54a4
ifrokz <dyfrok@gmail.com>
2018-04-15 09:02:48 +0200

    Add GET /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const { ObjectID } = require('mongodb');
 
 const app = express();
 const port = 3000;
@@ -22,6 +23,22 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos/:id', (req, res) => {
+    const _id = req.params.id;
+    if(!ObjectID.isValid(_id)){
+        res.status(404).send();
+        return;
+    }
+    Todo.findById(_id).then(todo=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.status(200).send(todo);
+    }).catch(err => { 
+        res.status(400).send()
+    });
+});
+
 app.get('/todos', (req, res) => {
     Todo.find().then( todos => {
         res.send({
@@ -31,6 +48,8 @@ app.get('/todos', (req, res) => {
         res.status(400).send(err);
     });
 });
+
+
 
 app.listen(port, () => {
     console.log('Started on port', port)
